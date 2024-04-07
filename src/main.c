bd42a047735ec2ba7d29e37388c4aae35ab10a60
sunerk <arnaulopez004@outlook.com>
2024-04-07 15:51:53 +0200

    Fixed: Now finds files (does not search inside directories atm)

--- src/main.c
+++ src/main.c
@@ -20,9 +20,7 @@ int main(int argc, char *argv[]) {
     // define the length to allocate
     size_t path_len = strlen(argv[1]);
     size_t file_len = strlen(argv[2]);
-    size_t extra_len = strlen("/");
-    size_t full_len = path_len + file_len + extra_len;
-    if (full_len >= MAX_LENGTH || file_len >= MAX_INPUT) {
+    if (path_len >= MAX_LENGTH || file_len >= MAX_INPUT) {
         printf("Error: path or file is too long!\n");
         return EXIT_FAILURE;
     }
@@ -30,33 +28,38 @@ int main(int argc, char *argv[]) {
     // allocate memory and copy in a safe way
     char *path = (char *) malloc((path_len + 1) * sizeof(char));
     char *filename = (char *) malloc((file_len + 1) * sizeof(char));
-    //char *complete_path = (char *) malloc((full_len + 1) * sizeof(char));
-    if (path == NULL || filename == NULL /*|| complete_path == NULL*/) {
+    if (path == NULL || filename == NULL) {
         fprintf(stderr, "Error: Failed memory allocation!\n");
+        free(path);
+        free(filename);
         return EXIT_FAILURE;
     }
-    strncpy(path, argv[1], path_len);
-    strncpy(filename, argv[2], file_len);
-    path[MAX_LENGTH] = '\0';
-    filename[MAX_INPUT] = '\0';
+    strncpy(path, argv[1], path_len + 1);
+    strncpy(filename, argv[2], file_len + 1);
 
     // verify directory
     d = opendir(path);
     if (!d) {
         printf("Error: no directory!\n");
+        free(path);
+        free(filename);
         return EXIT_FAILURE;
     }
 
     // search for file
     while ((dir = readdir(d)) != NULL) {
-        //printf("->%s\n", dir->d_name);
         if (strcmp(dir->d_name, filename) == 0) {
-            //printf("File {%s} found.\n", filename);
-            printf("%s/%s\n", getcwd(dir->d_name, MAX_LENGTH), filename);
+            printf("%s/%s\n", path, filename);
+            closedir(d);
+            free(path);
+            free(filename);
             return EXIT_SUCCESS;
         }
     }
+    printf("File not found.\n");
     closedir(d);
+    free(path);
+    free(filename);
 
     return EXIT_SUCCESS;
 }
